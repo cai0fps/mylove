3c0c9b9e1f5fe8ed1f07b1a03aec12c06a934b66
caio <80894086+cai0fps@users.noreply.github.com>
2026-03-06 15:50:42 -0300

    Create timer.js

---
+++ js/timer.js
@@ -0,0 +1,25 @@
+export function initTimer(startDateString) {
+    const startDate = new Date(startDateString);
+    
+    function updateCounter() {
+        const diff = new Date() - startDate;
+        if (diff < 0) return;
+
+        const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+        const months = Math.floor((diff % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44));
+        const days = Math.floor((diff % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+        document.getElementById('years').innerText = years;
+        document.getElementById('months').innerText = months;
+        document.getElementById('days').innerText = days;
+        document.getElementById('hours').innerText = hours;
+        document.getElementById('minutes').innerText = minutes;
+        document.getElementById('seconds').innerText = seconds;
+    }
+    
+    setInterval(updateCounter, 1000);
+    updateCounter();
+}
